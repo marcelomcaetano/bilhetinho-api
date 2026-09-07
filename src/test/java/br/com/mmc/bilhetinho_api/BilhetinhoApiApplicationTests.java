f700ef1da5ed8b38ace55ffc2660d6dba78ebe6a
Marcelo Caetano <mm.caetano@live.com>
2026-09-07 19:03:40 -0300

    feat: setup inicial do projeto Spring Boot 4.1.1 com Maven, JPA, PostgreSQL e Swagger

---
+++ src/test/java/br/com/mmc/bilhetinho_api/BilhetinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mmc.bilhetinho_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilhetinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
